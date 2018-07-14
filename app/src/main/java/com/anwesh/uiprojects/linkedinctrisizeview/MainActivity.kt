1615bb92d3859d7429b7e3797eb30410740a3f74
Anwesh43 <anweshthecool0@gmail.com>
2018-07-15 02:00:03 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedinctrisizeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedinctrisizeview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedinctrisizeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.litsview.LITSView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LITSView.create(this)
     }
 }
