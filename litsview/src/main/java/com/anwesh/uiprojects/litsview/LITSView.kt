d41a1ef279fce818124a8f2194e8721c95e9c7f9
Anwesh43 <anweshthecool0@gmail.com>
2018-07-15 01:28:40 +0530

    adding litsview module

--- litsview/src/main/java/com/anwesh/uiprojects/litsview/LITSView.kt
+++ litsview/src/main/java/com/anwesh/uiprojects/litsview/LITSView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.litsview
 /**
  * Created by anweshmishra on 15/07/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
