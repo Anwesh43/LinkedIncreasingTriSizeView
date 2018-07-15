ec0901e619e6447c45608565e9f116259a512512
Anwesh43 <anweshthecool0@gmail.com>
2018-07-15 20:22:51 +0530

    adding onAnimationCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedinctrisizeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedinctrisizeview/MainActivity.kt
@@ -2,12 +2,16 @@ package com.anwesh.uiprojects.linkedinctrisizeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.anwesh.uiprojects.litsview.LITSView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LITSView.create(this)
+        val view : LITSView = LITSView.create(this)
+        view.addOnCompleteListener {
+            Toast.makeText(this, "${it} animation completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
