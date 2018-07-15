a004f9bdafb13d93db838f9901e434d36753e270
Anwesh43 <anweshthecool0@gmail.com>
2018-07-15 21:36:50 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedinctrisizeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedinctrisizeview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedinctrisizeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import com.anwesh.uiprojects.litsview.LITSView
 
@@ -13,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         view.addOnCompleteListener {
             Toast.makeText(this, "${it} animation completed", Toast.LENGTH_SHORT).show()
         }
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
